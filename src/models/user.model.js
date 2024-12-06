14fbe0e1af0fd13842e6b2cbe46eaa077650461e
User <robin.mahe@epitech.digital>
2024-12-06 11:23:48 +0100

    add backend

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -5,3 +5,9 @@ const UserSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('User', UserSchema);
+// Définition du modèle User directement dans server.js
+const userSchema = new mongoose.Schema({
+  token: { type: String, required: true, unique: true },
+});
+
+const User = mongoose.model('User', userSchema);
